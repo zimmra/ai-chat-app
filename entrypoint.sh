0a3a2d7ca34561a7f9718bcf1bd609d21986b03b
zimmra <paytonz@icloud.com>
2023-03-08 15:38:27 -0800

    Cleanup and add variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
+# Copy config files if they don't exist
 if [ ! -d "/config" ]; then
     ln -s /app /config
 elif [ -z "$(ls -A /config)" ]; then
     cp -a /app/. /config/
 fi
-# cp -r /prisma /app
-# cp -r /public /app
-# cp -r /src /app
-# cp /next.config.mjs /app
-# cp /package.json /app
-# cp /shell.nix /app
-# cp /tsconfig.json /app
-# cp /yarn.lock /app
-# cd /app
+# Start application
 yarn start
